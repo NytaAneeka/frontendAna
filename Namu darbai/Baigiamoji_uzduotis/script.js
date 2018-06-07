fb0f0f8fb5638f98eefbd70d524f6dd25987ccba
Anastasia <anastasija9210@gmail.com>
2018-06-07 19:36:18 +0300

    Baigiamasis_update_06-07

--- Namu darbai/Baigiamoji_uzduotis/script.js
+++ Namu darbai/Baigiamoji_uzduotis/script.js
@@ -1,3 +1,4 @@
+//ADDS CLASS TO li TAG
 $(document).ready(function () {
     $('li.presentation').on('click', function () {
         $('li.presentation a').removeClass('active2');
@@ -11,18 +12,78 @@ $(document).ready(function () {
             $('.panel-heading').removeClass('active3');
             $(this).addClass('active3');
         }
+    });
 
+// COUNTS RUNNING NUMBERS
+    $(window).scroll(function() {
+        var hT = $('.numbers').offset().top,
+            hH = $('.numbers').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT+hH-wH)){
+            $('.Count').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 5000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+            $(window).unbind('scroll');
+        }
     });
-    $('.Count').each(function () {
-        $(this).prop('Counter',0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
+
+
+
+// COUNTS AND ANIMATES SKILL PROGRESS BARS
+
+    $(window).scroll(function() {
+        var hT = $('.skills').offset().top,
+            hH = $('.skills').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT+hH-wH)){
+            $(".web-design").animate({
+                    width: ["80%"]
+                },
+                {
+                    duration:800,
+                    step: function(now, fx) {
+                        if(fx.prop == 'width') {
+                            $(this).text(Math.round(now * 100) / 100 + '%');
+                        }
+                    }
+                }    );
+            $(".programming").animate({
+                width: "60%"
+            }, 800);
+            $(".html-css").animate({
+                width: "70%"
+            },800);
+            $(".wordpress").animate({
+                width: "100%"
+            },800);
+            $(".joomla").animate({
+                width: "40%"
+            },800);
+            // $('.count-skills').each(function () {
+            //     $(this).prop('Counter',0).animate({
+            //         Counter: $(this).text()
+            //     }, {
+            //         duration: 800,
+            //         easing: 'swing',
+            //         step: function (now) {
+            //             $(this).text(Math.ceil(now));
+            //         }
+            //     });
+            // });
+            $(window).unbind('scroll');
+        }
     });
+
 });
+
 
