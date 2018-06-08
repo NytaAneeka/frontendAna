ed1bd275d4b48e16f0ffab3d7d89d7742b5ad6c9
Anastasia <anastasija9210@gmail.com>
2018-06-08 20:21:39 +0300

    Baigiamasis_somewhat_final

--- Namu darbai/Baigiamoji_uzduotis/script/script.js
+++ Namu darbai/Baigiamoji_uzduotis/script/script.js
@@ -28,6 +28,58 @@ $(document).ready(function () {
     });
 
 
+    // SHOPPING BASKET DROPDOWN
+    $(document).mouseup(function(e)
+    {
+        var arr = [
+            '#toggleContainer'
+        ];
+        ensureHideOnOutsideClick(arr, e);
+    });
+
+    $(document).find('button.shopping-cart-icon').on('click', function (e) {
+        e.preventDefault();
+
+        var elm = $(document).find('#toggleContainer');
+
+        if (elm.hasClass('d-none')) {
+            elm.removeClass('d-none');
+        } else {
+            elm.addClass('d-none');
+        }
+        return false;
+    });
+
+    /**
+     * @param {array} arr
+     * @param {object} event
+     */
+    function ensureHideOnOutsideClick(arr, event) {
+        for (var i = 0; i < arr.length; i++) {
+            var container = $(document).find(arr[i]);
+
+            // if the target of the click isn't the container nor a descendant of the container
+            if (!container.is(event.target) && container.has(event.target).length === 0) {
+                container.addClass('d-none');
+            }
+        }
+    }
+
+
+    //BOARDER CHANGES WIDTH ON MOUSE HOVER
+    $('.hover-board').on('mouseenter',function () {
+        $(this).find('.green-border').animate({width:'30%'});
+    });
+    $('.hover-board').on('mouseleave',function () {
+        $(this).find('.green-border').animate({width:'15%'});
+    });
+
+
+
+    $('.dropdown').hover(function(){
+        $('.dropdown-toggle', this).trigger('click');
+    });
+
 });
 
 
